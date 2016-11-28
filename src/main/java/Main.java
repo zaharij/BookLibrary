5fe2dd69a9815b44ff413e500ff379d7d52cc691
Zakhar Yastrub <zahar.yastrub@gmail.com>
2016-11-28 11:50:03 +0200

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.mycompany.booklibrary.controller.LibraryManagingController;
+
+/**
+ *
+ * @author Zakhar
+ */
+public class Main {
+    
+    public static void main(String[] args){
+        LibraryManagingController libManage = new LibraryManagingController();
+        libManage.startManaging();
+    }
+}
---
+++ src/main/java/com/mycompany/booklibrary/model/entities/Book.java
@@ -0,0 +1,55 @@
+package com.mycompany.booklibrary.model.entities;
+
+/**
+ *
+ * @author Zakhar
+ */
+public class Book {
+    private String bookName;
+    private String author;
+    private int bookId;
+
+    /**
+     * @return the bookName
+     */
+    public String getBookName() {
+        return bookName;
+    }
+
+    /**
+     * @param bookName the bookName to set
+     */
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    /**
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    /**
+     * @return the bookId
+     */
+    public int getBookId() {
+        return bookId;
+    }
+
+    /**
+     * @param bookId the bookId to set
+     */
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+    
+    
+}
